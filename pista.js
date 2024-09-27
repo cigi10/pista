82ef0c92d39cb33f0573da4dc2be4046cc74cd7a
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2024-09-27 21:52:34 +0530

    added to repo

---
+++ pista.js
@@ -0,0 +1,37 @@
+function showDownloadPopup(imageId) {
+    
+    // Get the modal
+    var modal = document.getElementById("imageDownloadModal");
+    
+    var modalImage = document.getElementById("modalImage");
+    modalImage.src = imageId.src;
+
+    var modalImage = document.getElementById("modalHeader");
+    modalHeader.innerHTML = imageId.alt;
+    
+    
+    
+
+    // When the user clicks the button, open the modal 
+    
+    modal.style.display = "block";
+
+
+    
+}
+
+function downloadImage() {
+
+    var modalImage = document.getElementById("modalImage");
+    console.log(modalImage.src);
+    
+    const link = document.createElement('a');
+    //link.setAttribute('download', file);
+    link.download = "testimage.jpg";
+    link.href = modalImage.src;
+    //document.body.appendChild(link);
+    link.click();
+    link.remove();
+    //document.body.removeChild(link);
+    
+}
